936da4b2d17d823a7faccaa533c2ec9eea7bb49a
Ambarish Satheesh <ambarishsatheesh@hotmail.co.uk>
2018-11-16 19:28:39 +0000

    update

---
+++ webcam.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+
+#cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+_, first_frame = vc.read()
+first_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+first_gray = cv2.GaussianBlur(first_gray, (5, 5), 0)
+
+
+while rval:
+    rval, frame = vc.read()
+    #frame = cv2.flip(frame,1)
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+
+    difference = cv2.absdiff(first_gray, gray_frame)
+    _, difference = cv2.threshold(difference, 100, 255, cv2.THRESH_BINARY_INV)
+
+
+    #cv2.imshow("First frame", first_frame)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("difference", difference)
+
+
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
